8f436a6916b3a5e12c2e248a323939a88a0fcf6c
Rafael Fischer <rafaelsanfischer@gmail.com>
2023-10-31 15:26:04 -0300

    feat: working

--- invoices/use_cases.py
+++ invoices/use_cases.py
@@ -7,6 +7,6 @@ class InvoicesUseCases():
 
     invoice_model = Invoice()
 
-    def list_invoices_by_user(self, user_id: int) -> List[Invoice]:
+    def list_invoices_by_user(self, user_id: int):
         invoices = self.invoice_model.find_invoices_by_user_id(user_id=user_id)
         return invoices
--- invoices/views.py
+++ invoices/views.py
@@ -1,12 +1,50 @@
 from typing import List
 
+from django.db.models import Sum
+from rest_framework import serializers
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
-from invoices.models import Invoice
+from invoices import models
 from invoices.use_cases import InvoicesUseCases
 from users.models import CustomUser
+
+
+class ClientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Client
+        fields = '__all__'
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['email']
+
+
+class ServiceSerializer(serializers.ModelSerializer):
+    quantity = serializers.SerializerMethodField()
+
+    class Meta:
+        model = models.Service
+        fields = '__all__'
+
+    def get_quantity(self, service):
+        invoice_services = models.InvoiceService.objects.filter(
+            service=service)
+        print(invoice_services)
+        return 0
+
+
+class InvoicesSerializer(serializers.ModelSerializer):
+    client = ClientSerializer()
+    user = UserSerializer()
+    services = ServiceSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = models.Invoice
+        fields = '__all__'
 
 
 @api_view(['GET'])
@@ -17,6 +55,7 @@ def list_invoices(request):
     use_case = InvoicesUseCases()
 
     invoices = use_case.list_invoices_by_user(user_id=user_id)
-    print('************* ', invoices)
 
-    return Response('Hey')
+    serializer = InvoicesSerializer(invoices, many=True)
+
+    return Response(serializer.data)
